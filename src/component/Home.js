449ea836fcfb25483b1250657344a24a4ec4f773
akinenpallisanthosh <a.san92thosh@gmail.com>
2022-11-12 00:01:43 +0530

    Interface Upgrade

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,4 +1,4 @@
-import React, { useState  ,useRef   } from "react";
+import React, { useState  ,useRef ,useEffect  } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -14,9 +14,40 @@ function Home()
 const [tips , settips] = useState(["Relax And Stay cum"]);
 const [luckynumber , setluckymember] = useState(["It is your luck"]);
 const [luckycolor, setluckycolor] = useState(["Lucky Number is your luck"]);
+const [rashimy, setrashimy] = useState([""]);
+
+useEffect(() => { 
+if(localStorage.getItem('myrashi')!=null)
+{
+    let myrashilet =localStorage.getItem('myrashi');
+    setrashimy(myrashilet);
+   const options = {
+        method: 'POST',
+        url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+        params: {sign: myrashilet, day: 'today'},
+        headers: {
+          'X-RapidAPI-Key': '6375e655a2mshfdb992cc8f81f73p145582jsnb542bfa43fe2',
+          'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          settips([response.data.description]);
+          setluckymember([response.data.lucky_number]);
+          setluckycolor([response.data.color]);
+
+      }).catch(function (error) {
+          console.error(error);
+      });
+    
+
+}} ,[]);
 
  const Getresult = (e) => {
     console.log(e);
+    localStorage.setItem('myrashi',e);
+    setrashimy(e);
     const options = {
         method: 'POST',
         url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
@@ -42,11 +73,11 @@ const [luckycolor, setluckycolor] = useState(["Lucky Number is your luck"]);
         <>
             <Container className="bgtotal">
             
-                <div className="makecenter">
-                <Row>
+                <div className="makecenter" align="center">
+                <Row className="mt-2">
                         <Col>
                             <Form.Select aria-label="Default select example" className="" onChange={e =>Getresult(e.target.value)}>
-                            <option>Select You Rashi</option>
+                            <option value={(rashimy=="")?"":rashimy}>{(rashimy=="")?"Select Your Rashi":rashimy}</option>
                             <option value="aries">Aries</option>
                             <option value="taurus">Taurus</option>
                             <option value="gemini">Gemini</option>
@@ -64,7 +95,7 @@ const [luckycolor, setluckycolor] = useState(["Lucky Number is your luck"]);
                 </Row>
                 <Row className="mt-2">
                         
-                            <Col>
+                            <Col className="mt-2">
                                 <Card style={{ width: '18rem' }}>
                                 <Card.Body>
                                     <Card.Title>Tips & Advice</Card.Title>
@@ -75,7 +106,7 @@ const [luckycolor, setluckycolor] = useState(["Lucky Number is your luck"]);
                                 </Card.Body>
                                 </Card>
                             </Col>
-                            <Col>
+                            <Col className="mt-2">
                                 <Card style={{ width: '18rem' }}>
                                 <Card.Body>
                                     <Card.Title>Lucky Number</Card.Title>
@@ -86,7 +117,7 @@ const [luckycolor, setluckycolor] = useState(["Lucky Number is your luck"]);
                                 </Card.Body>
                                 </Card>
                             </Col>
-                            <Col>
+                            <Col className="mt-2">
                             <Card style={{ width: '18rem' }}>
                             <Card.Body>
                                 <Card.Title>Lucky Color</Card.Title>
